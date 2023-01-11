b1ffce6a706fdfb1cf170edcdc6f8fce6d0caec7
Ca <ca@wolffungame.com>
2023-01-11 12:06:29 +0700

    1.3.8

--- AddressablesManager/Docker.cs
+++ AddressablesManager/Docker.cs
@@ -11,57 +11,62 @@ namespace UnityEngine.AddressableAssets
         private static readonly HashSet<string> _dockedAssetToGameObject = new();
         private static bool _isInitialized;
 
-        public static void DockTo<T>(this UniTask<OperationResult<T>> operation, string assetKey,
+        public static UniTask<OperationResult<T>> DockTo<T>(this UniTask<OperationResult<T>> operation, string assetKey,
             Scene sceneDocker) where T : Object
         {
-            if (!GuardKey(assetKey, out var key))
-                return;
-            SubscribeSceneEvents();
-            if (!_sceneToAddress.TryGetValue(sceneDocker, out var addressList))
+            if (GuardKey(assetKey, out var key))
             {
-                addressList = CollectionPool<HashSet<string>, string>.Get();
-                addressList.Add(key);
-                _sceneToAddress.Add(sceneDocker, addressList);
+                SubscribeSceneEvents();
+                if (!_sceneToAddress.TryGetValue(sceneDocker, out var addressList))
+                {
+                    addressList = CollectionPool<HashSet<string>, string>.Get();
+                    addressList.Add(key);
+                    _sceneToAddress.Add(sceneDocker, addressList);
+                }
+                if (!addressList.Contains(key))
+                    addressList.Add(key);
             }
-
-            if (!addressList.Contains(key))
-                addressList.Add(key);
+            
+            return operation;
         }
 
         public static UniTask<OperationResult<T>> DockTo<T>(this UniTask<OperationResult<T>> operation,
             AssetReferenceT<T> reference, Scene sceneDocker) where T : Object
         {
             if (GuardKey(reference, out var key))
-                DockTo(operation, key, sceneDocker);
+                return DockTo(operation, key, sceneDocker);
             return operation;
         }
 
-        public static void DockTo<T>(this UniTask<OperationResult<T>> operation, string assetKey,
+        public static UniTask<OperationResult<T>> DockTo<T>(this UniTask<OperationResult<T>> operation, string assetKey,
             GameObject gameObject) where T : Object
         {
-            if (!GuardKey(assetKey, out var key))
-                return;
-
-            if (_dockedAssetToGameObject.Contains(assetKey))
+            if (GuardKey(assetKey, out var key))
             {
-                Debug.LogWarning(
-                    $"Not able to dock asset {key} to {gameObject.name} because it is already docked to another GameObject");
-                return;
+                if (!_dockedAssetToGameObject.Contains(assetKey))
+                {
+                    _dockedAssetToGameObject.Add(key);
+                    gameObject.OnDestroyTrigger(() =>
+                    {
+                        _dockedAssetToGameObject.Remove(key);
+                        ReleaseAsset(key);
+                    });
+                }
+                else
+                {
+                    Debug.LogWarning(
+                        $"Not able to dock asset {key} to {gameObject.name} because it is already docked to another GameObject");
+                }
             }
-            
-            _dockedAssetToGameObject.Add(key);
-            gameObject.OnDestroyTrigger(() =>
-            {
-                _dockedAssetToGameObject.Remove(key);
-                ReleaseAsset(key);
-            });
+          
+            return operation;
         }
 
         public static UniTask<OperationResult<T>> DockTo<T>(this UniTask<OperationResult<T>> operation,
             AssetReferenceT<T> reference, GameObject gameObject) where T : Object
         {
             if (GuardKey(reference, out var key))
-                DockTo(operation, key, gameObject);
+                return DockTo(operation, key, gameObject);
             return operation;
         }
 
