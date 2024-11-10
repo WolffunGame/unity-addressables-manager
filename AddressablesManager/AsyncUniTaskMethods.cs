e1b4a8c8ab497255fe7b5d5bd12c47b3c6fe1421
Ca <ca@wolffungame.com>
2024-11-10 10:09:17 +0700

    crap

--- AddressablesManager/AsyncUniTaskMethods.cs
+++ AddressablesManager/AsyncUniTaskMethods.cs
@@ -156,7 +156,7 @@ namespace UnityEngine.AddressableAssets
             try
             {
                 _asyncLoadingAssets.Add(key);
-                AsyncOperationHandle<TObject> operation = default;
+                UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<TObject> operation = default;
                 if (reference.OperationHandle.IsValid() && reference.OperationHandle.IsDone())
                     operation = reference.OperationHandle;
                 else
